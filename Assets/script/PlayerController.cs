044aa872dc1abaffff09ff93959a08cedfb8998b
xcui16-bit <xcui16@pratt.edu>
2026-01-29 15:28:15 -0500

    debug

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -3,20 +3,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 12;
+    public float gravity = 9.8f;
+    public float groundCHeckRadius = 0.15f;
+    public LayerMask groundLayer;//physic check
+
+    private bool isGrounded;
+    private Vector3 velocity;
+    private Transform feet;
 
     private CharacterController controller;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        feet = transform.Find("feet");
     }
 
     private void Update()
+    {
+        CheckIsGrounded();
+        //isGrounded = Physics.CheckSphere(feet.position, groundCHeckRadius, groundLayer);
+        Move();
+        ApplyGravity();
+    }
+
+    private void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move);
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundCHeckRadius, groundLayer);
+        Debug.Log($"IsGrounded: {isGrounded}");
+
+    }
+
+    private void ApplyGravity()
+    {
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        if(isGrounded)
+        velocity = Vector3.zero;
+
+        controller.Move(velocity);
     }
     }
