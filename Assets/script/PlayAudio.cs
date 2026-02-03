ea488d357fae83d4a6ec6337557cf0be481e2fc0
xcui16-bit <xcui16@pratt.edu>
2026-02-03 15:28:31 -0500

    audio script

--- Assets/script/PlayAudio.cs
+++ Assets/script/PlayAudio.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 
 public class PlayAudio : MonoBehaviour
 {
+   public float fadeTimeInSeconds;
+
    private AudioSource audio;
 
    private void Start()
@@ -10,12 +13,56 @@ public class PlayAudio : MonoBehaviour
     audio = GetComponent<AudioSource>();
    }
 
-   private void OnTriggerEnter(Collider other)//if player touched collider, trigger audio
+   private void OnTriggerEnter(Collider other)//if player touch collider, trigger audio
    {
     if(other.tag == "Player")
     {
-        audio.Play();
-        Debug.Log("PlayAudio");
+        //stop all current fade
+       StopAllCoroutines();
+       StartCoroutine(FadeAudio(true));
     }
+   }
+
+   private void OnTriggerExit(Collider other)//if player leave collider, audio stop
+   {
+    if(other.tag == "Player")
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeAudioOut());
+    }
+   }
+
+   private IEnumerator FadeAudio(bool fadeIn)
+   {
+    float timer = 0;
+    float start = fadeIn ? 0 : 1;
+    float end = fadeIn ? 1: 0;
+   
+    if(fadeIn) audio.Play();
+    while(timer < fadeTimeInSeconds)
+    {
+        audio.volume = Mathf.Lerp(0, 1, timer / fadeTimeInSeconds);
+        //timer divided by fade time.0:timer 1:fade time
+        //i.e. if fadeTime is 2s, and timer is 1s, 1/2=0.5(50% towards 2s)
+        timer += Time.deltaTime;
+        yield return null;
+    }
+    audio.volume = end;
+    if(!fadeIn) audio.Pause();
+   }
+
+   private IEnumerator FadeAudioOut()
+   {
+    float timer = 0;
+
+    while(timer < fadeTimeInSeconds)
+    {
+        audio.volume = Mathf.Lerp(1, 0, timer / fadeTimeInSeconds);
+        //timer divided by fade time.0:timer 1:fade time
+        timer += Time.deltaTime;
+        yield return null;
+    }
+    audio.volume = 0;
+    audio.Pause();
    }
 }
